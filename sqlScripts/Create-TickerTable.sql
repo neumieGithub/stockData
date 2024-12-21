20717cf0d002eb2cbe83b86208002ff10513bf68
Brian Neumann <neumie@neumie.com>
2024-12-21 16:46:44 -0500

    TickersTable Script

---
+++ sqlScripts/Create-TickerTable.sql
@@ -0,0 +1,9 @@
+CREATE OR REPLACE TABLE Tickers
+(
+    id          INT AUTO_INCREMENT  PRIMARY KEY,
+    ticker      VARCHAR(10) NOT NULL,
+    companyName VARCHAR(256)    NULL,
+    description VARCHAR(256)    NULL,
+    startDate   DATE            NULL,
+    endDate     DATE            NULL
+);
